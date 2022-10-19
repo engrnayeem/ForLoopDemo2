4b1a8229aed8162dd10e1ec6ca7ff41406503505
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-19 09:49:33 +0600

    code uploaded

---
+++ src/ForLoopDemo2/ForLoopdemo2.java
@@ -0,0 +1,22 @@
+
+package ForLoopDemo2;
+
+/**
+ *
+ * @author nayeem
+ */
+public class ForLoopdemo2 {
+    public static void main(String[]args)
+    {
+    
+    for(int x=1;x<=10;x++)
+    {
+    
+    System.out.println(x+".Bangladesh");
+    
+    }
+    
+    
+    }
+    
+}
